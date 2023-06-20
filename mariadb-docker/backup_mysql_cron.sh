a8f7227f70d89269ed666f29bafe0a3f19383d1a
Wen Guan <wguan.icedew@gmail.com>
2023-06-20 10:39:21 +0200

    update backup

--- mariadb-docker/backup_mysql_cron.sh
+++ mariadb-docker/backup_mysql_cron.sh
@@ -8,6 +8,7 @@ echo $MARIADB_BACKUP_RSA > /tmp/backup_id_rsa
 chmod 600 /tmp/backup_id_rsa
 
 # scp
-scp -q -i /tmp/backup_id_rsa  /tmp/${filename}  wguan@sdfrome001.sdf.slac.stanford.edu:/sdf/data/rubin/panda_jobs/panda_backup/
+# scp -q -i /tmp/backup_id_rsa  /tmp/${filename}  lsstsvc1@sdfrome001.sdf.slac.stanford.edu:/sdf/data/rubin/panda_jobs/panda_backup/
+scp -q -i /tmp/backup_id_rsa  /tmp/${filename} ${BACKUP_PATH}
 
 rm -fr /tmp/${filename} /tmp/backup_id_rsa
