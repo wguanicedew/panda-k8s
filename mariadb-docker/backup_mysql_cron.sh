d9820df5d3449474b208c63c7a5b3bd2a1def59d
Wen Guan <wguan.icedew@gmail.com>
2023-06-20 16:01:36 +0200

    update backup

--- mariadb-docker/backup_mysql_cron.sh
+++ mariadb-docker/backup_mysql_cron.sh
@@ -4,11 +4,12 @@ filename=${MARIADB_DATABASE}_${HOSTNAME}_$(date "+%Y.%m.%d-%H.%M.%S")
 # dump the db
 mysqldump -u root -p${MARIADB_ROOT_PASSWORD} --databases ${MARIADB_DATABASE} > /tmp/${filename}
 
-cp /opt/conf/id_rsa > /tmp/backup_id_rsa
+cp /opt/conf/id_rsa /tmp/backup_id_rsa
 chmod 600 /tmp/backup_id_rsa
 
 # scp
 # scp -q -i /tmp/backup_id_rsa  /tmp/${filename}  lsstsvc1@sdfrome001.sdf.slac.stanford.edu:/sdf/data/rubin/panda_jobs/panda_backup/
-scp -q -i /tmp/backup_id_rsa  /tmp/${filename} ${BACKUP_PATH}
+# scp -q -i /tmp/backup_id_rsa  /tmp/${filename} ${BACKUP_PATH}
+scp -q -o "ConnectTimeout 10"  -o "StrictHostKeyChecking no"  -o "UserKnownHostsFile /dev/null" -i /tmp/backup_id_rsa  /tmp/${filename} ${BACKUP_PATH}
 
 rm -fr /tmp/${filename} /tmp/backup_id_rsa
